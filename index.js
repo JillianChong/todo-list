4158c17c78ec08eef65509303d586cf45a3c996f
Jillian Chong <jc2886@cornell.edu>
2024-05-20 18:43:37 -0400

    Added table functionality & strikethrough element

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-function paint(color) {
-    const circle = document.getElementById('circleID');
-    circle.style = `background-color:${color}`;
-}
-
-function paintRandom() {
-    const circle = document.getElementById('circleID');
-
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-
-    circle.style = `background-color:${color}`;
-}
-
 function printInput() {
-    const firstName = document.getElementById('fname').value;
-    const lastName = document.getElementById('lname').value;
+    const taskName = document.getElementById('tname').value;
     // var paragraph = document.getElementById("outputid");
     // var text = document.createTextNode(firstName);
 
     // paragraph.appendChild(text);
 
-    const outputDiv = document.getElementById("outputid")
-    outputDiv.textContent = 'Output here:\n' + firstName + ' ' + lastName;
+    const outputDiv = document.getElementById("outputid");
+    outputDiv.textContent = 'Output here:\n' + taskName;
+}
+
+function addTask() {
+    const taskName = document.getElementById('tname').value;
+    var table = document.getElementById('TaskTableID');
+
+    var numRows = table.getElementsByTagName('tr').length;
+
+    const outputDiv = document.getElementById("outputid");
+    outputDiv.textContent = 'Output here:' + numRows;
+
+    var row = table.insertRow(numRows);
+    var cell = row.insertCell(0);
+
+    cell.innerHTML = taskName;
+
+    /* Adding event listener */
+    row.addEventListener("click", () => toggleStrikeThrough(numRows));
+}
+
+function toggleStrikeThrough(rowNum) {
+    /*
+        row_num: row number of table to strike
+    */
+
+    var table = document.getElementById('TaskTableID');
+    var rows = table.getElementsByTagName('tr');
+    var row = rows[rowNum];
+    var cell = row.cells[0];
+
+    if(cell.style.textDecoration == 'line-through') {
+        cell.style.textDecoration = 'none';
+    } else {
+        cell.style.textDecoration = 'line-through';
+    }
 }
