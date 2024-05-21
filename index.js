f1c7db4f585761603eaa86202b3907bbd994a435
Jillian Chong <jc2886@cornell.edu>
2024-05-20 20:34:45 -0400

    Added delete task function

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ function printInput() {
     outputDiv.textContent = 'Output here:\n' + taskName;
 }
 
-function addTask() {
+function addTask() {    
     const taskName = document.getElementById('tname').value;
+
+    if(taskName == '') {
+        window.alert("Enter a task!");
+        return;
+    }
+
     var table = document.getElementById('TaskTableID');
 
     var numRows = table.getElementsByTagName('tr').length;
@@ -22,24 +28,45 @@ function addTask() {
     var cell = row.insertCell(0);
 
     cell.innerHTML = taskName;
+    cell.style.textDecoration = 'none';
+
+    // clear input
+    document.getElementById('tname').value = "";
 
     /* Adding event listener */
-    row.addEventListener("click", () => toggleStrikeThrough(numRows));
+    row.addEventListener("click", () => toggleStrikeThrough(row));
 }
 
-function toggleStrikeThrough(rowNum) {
+function toggleStrikeThrough(row) {
     /*
         row_num: row number of table to strike
     */
 
-    var table = document.getElementById('TaskTableID');
-    var rows = table.getElementsByTagName('tr');
-    var row = rows[rowNum];
     var cell = row.cells[0];
 
     if(cell.style.textDecoration == 'line-through') {
         cell.style.textDecoration = 'none';
     } else {
         cell.style.textDecoration = 'line-through';
+    } 
+}
+
+function deleteTasks() {
+    var table = document.getElementById('TaskTableID');
+    var rows = table.getElementsByTagName('tr');
+    const indexesToRemove = [];
+
+    for(var i = 0; i < rows.length; i++) {
+        var cell = rows[i].cells[0];
+        if(cell.style.textDecoration == 'line-through') {
+            indexesToRemove.push(i);
+        } else if(cell.innerHTML == '') {
+            indexesToRemove.push(i);
+        }
+    }
+
+    for(var i = indexesToRemove.length - 1; i >= 0; i--) {
+        var index = indexesToRemove[i];
+        rows[index].remove();
     }
 }
