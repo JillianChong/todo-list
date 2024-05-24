8b87947baae656131a9d6032a9ce0b6ea110252b
Jillian Chong <jc2886@cornell.edu>
2024-05-24 11:44:17 -0400

    Added information tab

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function printInput() {
     // paragraph.appendChild(text);
 
     const outputDiv = document.getElementById("outputid");
-    outputDiv.textContent = 'Output here:\n' + taskName;
+    outputDiv.textContent = 'Tasks left:\n' + taskName;
 }
 
 function addTask() {    
     const taskName = document.getElementById('tname').value;
+    const dueDate = document.getElementById('date').value;
 
     if(taskName == '') {
         window.alert("Enter a task!");
@@ -18,37 +19,108 @@ function addTask() {
     }
 
     var table = document.getElementById('TaskTableID');
-
     var numRows = table.getElementsByTagName('tr').length;
 
     const outputDiv = document.getElementById("outputid");
-    outputDiv.textContent = 'Output here:' + numRows;
+    outputDiv.textContent = 'Tasks left:\n' + numRows;
 
     var row = table.insertRow(numRows);
-    var cell = row.insertCell(0);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
 
-    cell.innerHTML = taskName;
-    cell.style.textDecoration = 'none';
+    row.id = 'RowID';
+
+    if(document.getElementById('GreenPriority').checked) {
+        cell2.bgColor = "Green";
+    } else if(document.getElementById('YellowPriority').checked) {
+        cell2.bgColor = "Yellow";
+    } else if(document.getElementById('RedPriority').checked) {
+        cell2.bgColor = "Red";
+    } else {
+        cell2.bgColor = "White";
+    }
+
+    cell0.innerHTML = taskName;
+    cell0.style.textDecoration = 'none';
+
+    cell1.innerHTML = dueDate;
+
+    const infoTxt = document.getElementById('information').value;
+    generateInformationDiv(infoTxt, numRows);
+
+    const infoButton = document.createElement('button');
+    infoButton.textContent = 'Info';
+    cell3.appendChild(infoButton);
+    infoButton.addEventListener('click', () => toggleInformationDiv(numRows + 1));
 
     // clear input
     document.getElementById('tname').value = "";
+    document.getElementById('date').value = "";
+    document.getElementById('GreenPriority').checked = false;
+    document.getElementById('YellowPriority').checked = false;
+    document.getElementById('RedPriority').checked = false;
+    document.getElementById('information').value = "";
 
     /* Adding event listener */
-    row.addEventListener("click", () => toggleStrikeThrough(row));
+    cell0.addEventListener("click", () => toggleStrikeThrough(row));
+    cell1.addEventListener("click", () => toggleStrikeThrough(row));
+}
+
+function generateInformationDiv(infoTxt) {
+    var table = document.getElementById('TaskTableID');
+    var numRows = table.getElementsByTagName('tr').length;
+
+    var infoRow = table.insertRow(numRows);
+    var cell = infoRow.insertCell(0);
+    cell.colSpan = 4;
+
+    cell.innerHTML = infoTxt;
+
+    cell.id = 'InfoParagraphID';
+    
+    // infoParagraph.id = 'InfoParagraphID' + numRows;
+    cell.style.display = 'none';
+}
+
+function toggleInformationDiv(rowNum) {
+    //var infoParagraph = document.getElementById("InfoParagraphID");
+    var table = document.getElementById('TaskTableID');
+    var numRows = table.getElementsByTagName('tr')
+    var infoParagraph = table.rows[rowNum].cells[0];
+
+    if(infoParagraph.style.display == "none") {
+        infoParagraph.style.display = "";
+    } else {
+        infoParagraph.style.display = "none";
+    }
+
 }
 
 function toggleStrikeThrough(row) {
     /*
         row_num: row number of table to strike
     */
+    var striked = false;
+    var cell1 = row.cells[0];
 
-    var cell = row.cells[0];
-
-    if(cell.style.textDecoration == 'line-through') {
-        cell.style.textDecoration = 'none';
+    if(cell1.style.textDecoration == 'line-through') {
+        striked = true;
     } else {
-        cell.style.textDecoration = 'line-through';
-    } 
+        striked = false;
+        //numRows--;
+    }
+
+    for(var i = 0; i < row.cells.length; i++) {
+        var cell = row.cells[i];
+
+        if(striked) {
+            cell.style.textDecoration = 'none';
+        } else {
+            cell.style.textDecoration = 'line-through';
+        } 
+    }
 }
 
 function deleteTasks() {
@@ -56,17 +128,24 @@ function deleteTasks() {
     var rows = table.getElementsByTagName('tr');
     const indexesToRemove = [];
 
-    for(var i = 0; i < rows.length; i++) {
+    for(var i = 1; i < rows.length; i += 2) {
         var cell = rows[i].cells[0];
         if(cell.style.textDecoration == 'line-through') {
             indexesToRemove.push(i);
+            indexesToRemove.push(i+1);
         } else if(cell.innerHTML == '') {
             indexesToRemove.push(i);
+            indexesToRemove.push(i+1);
         }
     }
+
+    console.log(indexesToRemove);
 
     for(var i = indexesToRemove.length - 1; i >= 0; i--) {
         var index = indexesToRemove[i];
         rows[index].remove();
     }
+
+    const outputDiv = document.getElementById("outputid");
+    outputDiv.textContent = 'Tasks left:\n' + (rows.length - 1);
 }
